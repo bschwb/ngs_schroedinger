d90e0c0f4310c16c9b445085cdc8170468c01ee5
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2018-03-08 21:05:24 +0100

    Use unit_square geometry

--- schroedinger.py
+++ schroedinger.py
@@ -1,3 +1,8 @@
 """ Simulate the time-independet Schrödingers Equation"""
 
 import ngsolve as ngs
+from netgen.geom2d import unit_square
+
+mesh = ngs.Mesh(unit_square.GenerateMesh(maxh=0.2))
+
+Draw(mesh)
