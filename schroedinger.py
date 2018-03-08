8a2ba4024843e099cd3e478d6c8b1aa93caef3b3
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2018-03-08 21:18:44 +0100

    Add random initial condition

--- schroedinger.py
+++ schroedinger.py
@@ -5,8 +5,10 @@ from scipy.constants import m_e, hbar
 import ngsolve as ngs
 from netgen.geom2d import unit_square
 
+from random import random
 
-mesh = ngs.Mesh(unit_square.GenerateMesh(maxh=0.2))
+
+mesh = ngs.Mesh(unit_square.GenerateMesh(maxh=0.05))
 
 fes = ngs.H1(mesh, order=1, dirichlet=[1,2,3,4], complex=True)
 
@@ -21,4 +23,10 @@ m = ngs.BilinearForm(fes)
 m += ngs.SymbolicBFI(u * v)
 m.Assemble()
 
-Draw(mesh)
+gf_psi = ngs.GridFunction(fes)
+
+freedofs = fes.FreeDofs()
+for i in range(len(gf_psi.vec)):
+    gf_psi.vec[i] = random() if freedofs[i] else 0
+
+Draw(gf_psi)
